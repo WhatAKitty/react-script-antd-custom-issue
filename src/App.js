794be52b8c12870067ef4806e7994a36ef4c770a
WhatAKitty <104xuqiang@163.com>
2017-11-01 16:53:53 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Button } from 'antd';
+import logo from './logo.svg';
+
+class App extends Component {
+  render() {
+    return (
+      <Button type="primary">Primary</Button>
+    );
+  }
+}
+
+export default App;
